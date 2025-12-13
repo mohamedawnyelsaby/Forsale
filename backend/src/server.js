1b8eeb191dd610db8b28a32ca3161088a2c28c1b
Mohamed awny <55304205+mohamedawnyelsaby@users.noreply.github.com>
2025-12-13 20:01:12 +0200

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
-import app from "./app.js";
+import app from "./src/app.js";
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Forsale Backend running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
