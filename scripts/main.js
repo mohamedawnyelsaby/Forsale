bce154d6f7f6930b4ce46c1590a2be56e30934de
Mohamed awny <55304205+mohamedawnyelsaby@users.noreply.github.com>
2025-12-21 22:14:26 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,10 @@
 // PATH: scripts/main.js
 
 import { App } from "./core/app.init.js";
+import I18N from "../i18n/loader.js";
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
+    const lang = navigator.language.startsWith("ar") ? "ar" : "en";
+    await I18N.load(lang);
     App.start();
 });
