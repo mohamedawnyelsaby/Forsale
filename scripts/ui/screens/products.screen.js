d031a4ce608a81dd44ad05bf2a2fdc4e4ef0656e
Mohamed awny <55304205+mohamedawnyelsaby@users.noreply.github.com>
2025-12-21 23:05:17 +0200

    Create products.screen.js

---
+++ scripts/ui/screens/products.screen.js
@@ -0,0 +1,10 @@
+// PATH: scripts/ui/screens/products.screen.js
+
+export function ProductsScreen(container) {
+  container.innerHTML = `
+    <section class="screen">
+      <h1>Marketplace</h1>
+      <div id="products-list"></div>
+    </section>
+  `;
+}
