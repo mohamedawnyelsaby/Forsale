af926556574108c26ced29633965a6402430aacd
Mohamed awny <55304205+mohamedawnyelsaby@users.noreply.github.com>
2025-12-21 22:27:16 +0200

    Update app.init.js

--- scripts/core/app.init.js
+++ scripts/core/app.init.js
@@ -3,11 +3,13 @@
 import { UIState } from "./state.store.js";
 import { UIRouter } from "./router.ui.js";
 import { Tabs } from "../ui/tabs.ui.js";
+import { ProductsUI } from "../ui/products.ui.js";
 
 export const App = {
     start() {
         UIState.init();
         UIRouter.init();
         Tabs.init();
+        ProductsUI.init("home-screen");
     }
 };
