1a375b80db6980ec0f34bd6b9487b9df5738a1be
Mohamed awny <55304205+mohamedawnyelsaby@users.noreply.github.com>
2025-12-21 23:06:48 +0200

    Create profile.screen.js

---
+++ scripts/ui/screens/profile.screen.js
@@ -0,0 +1,10 @@
+// PATH: scripts/ui/screens/profile.screen.js
+
+export function ProfileScreen(container) {
+  container.innerHTML = `
+    <section class="screen">
+      <h1>Profile</h1>
+      <p>User account & settings</p>
+    </section>
+  `;
+}
